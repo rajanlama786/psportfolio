1c17e2ad76a1110ac30c91a0c5056ca20fb21091
Rajan Lama <rajan.lama786@gmail.com>
2020-08-27 20:00:16 +0545

    image linking

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const About = () => (
+    <div>
+        <h1>About</h1>
+        <Link href="/">
+            <a>Go To Home</a>
+        </Link>
+        <p>A Javascript programmer</p>
+        <img src="/images/Photo-0034.jpg" alt="rajan lama photo" />
+    </div>
+
+)
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
-const Index = () => <div> Welcome to the Home page. </div>;
+import Link from "next/link";
+
+const Index = () => (
+    <div>
+        <h1>Home</h1>
+        <Link href="/about">
+            <a>Go To Home</a>
+        </Link>
+        <p> Welcome to the Home page. </p>
+    </div>
+)
+
 
 export default Index;
